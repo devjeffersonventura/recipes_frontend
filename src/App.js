b74f52dfa6a3f18054e6dc8b154b668abd6933fe
Jefferson <jefferson@educationjourney.com>
2025-02-10 20:46:49 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Login from './components/Login'; // Importe o componente Login correto
+import Dashboard from './components/Dashboard';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/receitas" element={<Receitas />} />
           <Route path="/login" element={<Login />} /> {/* Use o componente Login importado */}
+          <Route path="/dashboard" element={<Dashboard />} /> {/* Rota do Dashboard */}
+          <Route path="/cadastro-usuario" element={<CadastroUsuario />} /> {/* Rota de Cadastro de Usuário */}
+          <Route path="/cadastro-receitas" element={<CadastroReceitas />} /> {/* Rota de Cadastro de Receitas */}
         </Routes>
       </div>
     </Router>
@@ -25,6 +29,14 @@ function Home() {
 
 function Receitas() {
   return <h1>Receitas</h1>;
+}
+
+function CadastroUsuario() {
+  return <h1>Cadastro de Usuário</h1>; // Página de Cadastro de Usuário
+}
+
+function CadastroReceitas() {
+  return <h1>Cadastro de Receitas</h1>; // Página de Cadastro de Receitas
 }
 
 export default App;
