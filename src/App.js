27745e76f4affa39401308431f6d0db4bbefd52b
Jefferson <jefferson@educationjourney.com>
2025-02-10 21:40:07 -0300

    add auth context and protected route

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Login from './components/Login'; // Importe o componente Login correto
 import Dashboard from './components/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute'; // Importe o ProtectedRoute
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext'; // Importe o AuthProvider
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/receitas" element={<Receitas />} />
-          <Route path="/login" element={<Login />} /> {/* Use o componente Login importado */}
-          <Route path="/dashboard" element={<Dashboard />} /> {/* Rota do Dashboard */}
-          <Route path="/cadastro-usuario" element={<CadastroUsuario />} /> {/* Rota de Cadastro de Usuário */}
-          <Route path="/cadastro-receitas" element={<CadastroReceitas />} /> {/* Rota de Cadastro de Receitas */}
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} /> {/* Use o componente Login importado */}
+            <Route path="/receitas" element={<Receitas />} />
+
+            <Route element={<ProtectedRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} /> {/* Rota do Dashboard */}
+              <Route path="/cadastro-usuario" element={<CadastroUsuario />} /> {/* Rota de Cadastro de Usuário */}
+              <Route path="/cadastro-receitas" element={<CadastroReceitas />} /> {/* Rota de Cadastro de Receitas */}
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
